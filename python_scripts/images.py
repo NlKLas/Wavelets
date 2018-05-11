1c15f658ff1c8cd35a83b0a28396d9e807820edc
Simon Cordes <s.sedroc@gmail.com>
2018-05-11 20:03:22 +0200

    Made a small change to the image code. It now more closely resembles the methods detailed in the draft.

--- python_scripts/images.py
+++ python_scripts/images.py
@@ -5,52 +5,41 @@ import matplotlib.pyplot as plt
 
 
 def DecompositionStep(C):
-    #print('\t'+str(C))
-    h = C.size
     C_prime = np.empty_like(C)
+    h = C.size
     for i in range(0,h//2):
-        # sqrt(2) * [Mittelwert von C[2*i-1] und C[2*i]]
         C_prime[i] = (C[2*i]+C[2*i+1])/(np.sqrt(2))
-        # sqrt(2) * [Abstand von C[2*i-1] bzw. C[2*i] vom Mittelwert]
         C_prime[h//2+i] = (C[2*i]-C[2*i+1])/(np.sqrt(2))
     
-    #print('\t'+str(C_prime))
+    return C_prime
+
+# C is assumed to be an array of coefficients corresponding to the normalized (!) basis
+def Decomposition(C):
+    C_prime = C.copy()
+    h = C.size
+    while h > 1:
+        C_prime[0:h] = DecompositionStep(C_prime[0:h])
+        h = h//2
+    
     return C_prime
 
 
-def Decomposition(C):
-    #print(C)
-    h = C.size
-    # Multipliziere Alles mit 2^{-j/2} = 1/sqrt(2^j) = 1/sqrt(h)
-    C = C/np.sqrt(h)
-    while h > 1:
-        #print(C)
-        C[0:h] = DecompositionStep(C[0:h])
-        h = h//2
+def SynthesisStep(C_prime):
+    C = np.empty_like(C_prime)
+    h = C_prime.size // 2
+    for i in range(0, h):
+        C[2*i] = (C_prime[i]+C_prime[h+i])/(np.sqrt(2))
+        C[2*i+1] = (C_prime[i]-C_prime[h+i])/(np.sqrt(2))
     
-    #print(C)
     return C
 
 
-def SynthesisStep(C):
-    h = C.size
-    C_prime = np.empty_like(C)
-    for i in range(0, h//2):
-        C_prime[2*i] = C[i]+C[h//2+i]
-        C_prime[2*i+1] = C[i]-C[h//2+i]
-    
-    return C_prime
-
-
-
-def Synthesis(C):
-    h = C.size
-    
-    i = 2
-    while i <= h:
-        C[0:i] = SynthesisStep(C[0:i])
-        C[i:h] = np.sqrt(2)*C[i:h]
-        i = 2*i
+def Synthesis(C_prime):
+    C = C_prime.copy()
+    h = 1
+    while h < C_prime.size:
+        h = 2*h
+        C[0:h] = SynthesisStep(C[0:h])
     
     return C
 
@@ -68,7 +57,6 @@ def DecompositionStepX(C):
 
 def DecompositionX(C):
     h = C.shape[1]
-    C = C/np.sqrt(h)
     while h > 1:
         C[:,0:h] = DecompositionStepX(C[:,0:h])
         h = h//2
@@ -84,18 +72,17 @@ def DecompositionStepY(C):
 
 def DecompositionY(C):
     h = C.shape[0]
-    C = C/np.sqrt(h)
     while h > 1:
         C[0:h,:] = DecompositionStepY(C[0:h,:])
         h = h//2
     return C
 
 def SynthesisStepX(C):
-    h = C.shape[1]
+    h = C.shape[1] // 2
     C_prime = np.empty_like(C)
-    for i in range(0, h//2):
-        C_prime[:,2*i] = C[:,i]+C[:,h//2+i]
-        C_prime[:,2*i+1] = C[:,i]-C[:,h//2+i]
+    for i in range(0, h):
+        C_prime[:,2*i] = (C[:,i]+C[:,h+i])/np.sqrt(2)
+        C_prime[:,2*i+1] = (C[:,i]-C[:,h+i])/np.sqrt(2)
     return C_prime
 
 def SynthesisX(C):
@@ -103,16 +90,15 @@ def SynthesisX(C):
     i = 2
     while i <= h:
         C[:,0:i] = SynthesisStepX(C[:,0:i])
-        C[:,i:h] = np.sqrt(2)*C[:,i:h]
         i = 2*i
     return C
 
 def SynthesisStepY(C):
-    h = C.shape[0]
+    h = C.shape[0] // 2
     C_prime = np.empty_like(C)
-    for i in range(0, h//2):
-        C_prime[2*i,:] = C[i,:]+C[h//2+i,:]
-        C_prime[2*i+1,:] = C[i,:]-C[h//2+i,:]
+    for i in range(0, h):
+        C_prime[2*i,:] = (C[i,:]+C[h+i,:])/np.sqrt(2)
+        C_prime[2*i+1,:] = (C[i,:]-C[h+i,:])/np.sqrt(2)
     return C_prime
 
 def SynthesisY(C):
@@ -120,7 +106,6 @@ def SynthesisY(C):
     i = 2
     while i <= h:
         C[0:i,:] = SynthesisStepY(C[0:i,:])
-        C[i:h,:] = np.sqrt(2)*C[i:h,:]
         i = 2*i
     return C
 
@@ -136,7 +121,7 @@ def StandardSynthesis(C):
     img = SynthesisX(img)
     return img
     
-#TODO: Implement the non-standard decompostition and synthesis
+
 '''
 I will assume a square image (if in doubt, add padding...)
 '''
@@ -144,60 +129,59 @@ def NonstandardDecomposition(img):
     C = img.copy()
     
     h = C.shape[0]
-    C = C/h
     while h > 1:
         C[0:h,0:h] = DecompositionStepX(C[0:h,0:h])
         C[0:h,0:h] = DecompositionStepY(C[0:h,0:h])
-        plt.imshow(C, cmap='gray')
-        plt.show()
+        #plt.imshow(C, cmap='gray')
+        #plt.show()
         h = h//2
     return C
 
 def NonstandardSynthesis(C):
     img = C.copy()
     
-    h = img.shape[0]
-    i = 2
-    while i <= h:
-        img[0:i,0:i] = SynthesisStepY(img[0:i,0:i])
-        
-        img[0:i,0:i] = SynthesisStepX(img[0:i,0:i])
-        
-        img[i:h,0:i] = 2*img[i:h,0:i]
-        img[i:h,i:h] = 2*img[i:h,i:h]
-        img[0:i,i:h] = 2*img[0:i,i:h]
-        
-        plt.imshow(img, cmap='gray')
-        plt.show()
-        i = 2*i
+    h = 1
+    while h < C.shape[0]:
+        h = 2*h
+        img[0:h,0:h] = SynthesisStepY(img[0:h,0:h])
+        img[0:h,0:h] = SynthesisStepX(img[0:h,0:h])
+        #plt.imshow(img, cmap='gray')
+        #plt.show()
     return img
 
 if __name__ == "__main__":
     np.set_printoptions(linewidth=200)
     C = np.array([9.0,7,3,5,7,4,9,0])
     
+    #normalize:
+    C = C/np.sqrt(C.size)
     print(C)    
     
-    print(Decomposition(C))
+    B = Decomposition(C)
+    print(B)
     
-    print(Synthesis(Decomposition(C)))
+    A = Synthesis(B)
+    print(A)
     
     lena = np.asarray(PIL.Image.open('lena.ppm'))    
     lena = np.mean(lena, axis=2)
     plt.imshow(lena, cmap='gray')
     plt.show()
+    #currently unnormalized...
     '''
     plt.imshow(StandardDecomposition(lena), cmap='gray')
     plt.show()
     
     plt.imshow(StandardSynthesis(StandardDecomposition(lena)), cmap='gray')
     plt.show()
-    '''
+    
     plt.imshow(NonstandardDecomposition(lena), cmap='gray')
     plt.show()
-    
+    '''
     plt.imshow(NonstandardSynthesis(NonstandardDecomposition(lena)), cmap='gray')
     plt.show()
+    
+    
     '''
     mona = np.asarray(PIL.Image.open('mona.jpg'))
     mona = np.mean(mona, axis=2)
